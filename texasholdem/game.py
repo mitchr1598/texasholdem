6b8301fe5d23e51a73a00faefbe314b74a13b5b0
mitchr1598 <a1688803@student.adelaide.edu.au>
2022-01-12 23:12:30 +1030

    added PokerCollection. added range in new players module

--- texasholdem/game.py
+++ texasholdem/game.py
@@ -1,28 +1,60 @@
 from playingcards import Card, Deck, CardCollection
+from dataclasses import dataclass
+from collections import Counter
 
 
-class TexasHand(CardCollection):
+class PokerCollection(CardCollection):
+    """
+    A wrapper for CardCollection. Introduces properties important to poker
+    """
+    def __init__(self, card, maximum):
+        super().__init__(card, maximum, ordered=True, reverse_order=True)
+
+    @property
+    def tone(self):
+        return len(set([card.suit for card in self.cards]))
+
+    @property
+    def paired(self):
+        return 2 in Counter([c.rank for c in self.cards]).values()
+
+
+class TexasHand(PokerCollection):
     def __init__(self, cards: list[Card]):
         super().__init__(cards, maximum=2)
-        self.cards.sort(reverse=True)
 
 
-class Flop(CardCollection):
+class Flop(PokerCollection):
     def __init__(self, cards: list[Card]):
         super().__init__(cards, maximum=3)
-        self.cards.sort(reverse=True)
 
 
-class Turn(CardCollection):
+class Turn(PokerCollection):
     def __init__(self, cards: list[Card]):
         super().__init__(cards, maximum=1)
-        self.cards.sort(reverse=True)
 
 
-class River(CardCollection):
+class River(PokerCollection):
     def __init__(self, cards: list[Card]):
         super().__init__(cards, maximum=1)
-        self.cards.sort(reverse=True)
+
+
+@dataclass
+class Board:
+    flop: Flop
+    turn: Turn = None
+    river: River = None
+
+    def __post_init__(self):
+        self.cards = PokerCollection(self.flop + self.turn + [self.river.cards[0]], maximum=5)
+
+    @classmethod
+    def from_string(cls, board_str):
+        board_str = board_str.replace(' ', '')
+        flop = Flop.from_string(board_str[:6])
+        turn = Turn.from_string(board_str[6:8]) if len(board_str) > 6 else None
+        river = River.from_string(board_str[8:]) if len(board_str) > 8 else None
+        return Board(flop, turn, river)
 
 
 def main():
