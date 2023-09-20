b6dbe63e02537934f5a7c515801b1b4de5e0add2
mitchr1598 <a1688803@student.adelaide.edu.au>
2023-09-20 15:02:03 +0930

    adjusted init imports

--- texasholdem/__init__.py
+++ texasholdem/__init__.py
@@ -1,3 +1,3 @@
-from .texas_collections import TexasHand, Flop, Turn, River, Board, TexasDeck
-from .properties import TexasHandProperties
-from . import sampling
+from texasholdem.texas_collections import TexasHand, Flop, Turn, River, Board, TexasDeck
+from texasholdem.properties import TexasHandProperties
+from texasholdem import sampling
